5aacfbc35ac0f16b277414f12fa8e092cb844bfb
rkoko <rinda.ko@gmail.com>
2017-10-03 20:59:56 -0400

    get arrows working and move quote body to own component

--- app/javascript/quotes/components/QuotesBody.jsx
+++ app/javascript/quotes/components/QuotesBody.jsx
@@ -1,1 +1,16 @@
 import React from 'react';
+
+const QuotesBody = ({quote}) => (
+  <div className='quote'>
+    <div className='quote-open'>“</div>
+    <div className='quote-close'>”</div>
+    <div className='quote-text'>
+      {quote.text}
+    </div>
+    <div className='quote-author'>
+      <em>— {quote.author}</em>
+    </div>
+  </div>
+)
+
+export default QuotesBody
--- app/javascript/quotes/components/QuotesDisplay.jsx
+++ app/javascript/quotes/components/QuotesDisplay.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import queryString from 'query-string';
 import axios from 'axios';
 import QuoteNavigation from './QuoteNavigation';
+import QuotesBody from './QuotesBody'
 
 class QuotesDisplay extends React.Component {
   constructor() {
@@ -57,21 +58,12 @@ class QuotesDisplay extends React.Component {
           {this.state.fireRedirect &&
             <Redirect to={'/'} />
           }
+          {previousQuoteId != null ?   <QuoteNavigation direction='previous' otherQuoteId={previousQuoteId} /> : null }
 
-          <QuoteNavigation direction='previous' otherQuoteId={previousQuoteId} />
+          <QuotesBody quote={this.state.quote} />
 
-          <div className='quote'>
-            <div className='quote-open'>“</div>
-            <div className='quote-close'>”</div>
-            <div className='quote-text'>
-              {quote.text}
-            </div>
-            <div className='quote-author'>
-              <em>— {quote.author}</em>
-            </div>
-          </div>
+          {nextQuoteId != null ? <QuoteNavigation direction='next' otherQuoteId={nextQuoteId} /> : null}
 
-          <QuoteNavigation direction='next' otherQuoteId={nextQuoteId} />
 
         </div>
         {this.state.quote.id !== parseInt(this.props.startingQuoteId, 10) &&
